91fb65535e05189fef4a3f8bd17bba2b2d7f1831
LayNath242 <laynath242@gmail.com>
2022-04-11 13:29:55 +0700

    seperate runtime

---
+++ backend/src/operatons/runtime.js
@@ -0,0 +1,38 @@
+const Sentry = require('@sentry/node');
+
+const constants = require('../config');
+const utils = require('../utils');
+const logger = require('../utils/logger');
+
+Sentry.init({
+    dsn: constants.SENTRY,
+    tracesSampleRate: 1.0,
+  });
+  
+
+async function storeMetadata(api, blockNumber, blockHash, specName, specVersion, timestamp){
+    const metadata = await api.rpc.state.getMetadata(blockHash);
+  
+    try {
+      const runtimeCol = await utils.db.getRuntimeColCollection();
+      await runtimeCol.insertOne({
+          blockNumber,
+          specName,
+          specVersion,
+          metadata_version: Object.keys(metadata.toHuman().metadata)[0],
+          magic_number: metadata.magicNumber.toHuman(),
+          metadata: metadata.toHuman().metadata,
+          timestamp,
+      })
+      logger.info(`Got runtime metadata at ${blockHash}!`);
+    } catch (error) {
+      logger.error(`Error fetching runtime metadata at ${blockHash}: ${JSON.stringify(error)}`);
+      const scope = new Sentry.Scope();
+      scope.setTag('blockNumber', blockNumber);
+      Sentry.captureException(error, scope);
+    }
+  };
+
+  module.exports = {
+    storeMetadata
+  }
