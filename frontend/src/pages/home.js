b59733d41d076393a4976123777cdae4bfcacca8
Densovan <sarimramata@gmail.com>
2022-06-05 19:51:19 +0700

    fix enter search

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -22,19 +22,19 @@ export default function Home() {
   useEffect(() => {
     let unsubscribeAll = null;
 
-    bestNumber(number => {
-      setBlockNumber(number.toNumber().toLocaleString('en-US'));
-    })
-    bestNumberFinalized(number => {
-      setBlockNumberFinalized(number.toNumber().toLocaleString('en-US'));
-    })
-    validatorsData(data => {
+    bestNumber((number) => {
+      setBlockNumber(number.toNumber().toLocaleString("en-US"));
+    });
+    bestNumberFinalized((number) => {
+      setBlockNumberFinalized(number.toNumber().toLocaleString("en-US"));
+    });
+    validatorsData((data) => {
       setValidators(data.toHuman());
     })
-    .then(unsub => {
-      unsubscribeAll = unsub;
-    })
-    .catch(console.error);
+      .then((unsub) => {
+        unsubscribeAll = unsub;
+      })
+      .catch(console.error);
 
     return () => unsubscribeAll && unsubscribeAll();
   }, [bestNumber, bestNumberFinalized, validatorsData]);
@@ -45,35 +45,36 @@ export default function Home() {
       fetch(`${process.env.REACT_APP_API}/transfer/all/1`),
       fetch(`${process.env.REACT_APP_API}/totals`),
       fetch(`${process.env.REACT_APP_API}/staking/status`),
-      fetch(`${process.env.REACT_APP_API}/totals/lock_balances`)
+      fetch(`${process.env.REACT_APP_API}/totals/lock_balances`),
     ])
-    .then(async([a, b, c, d, e]) => {
-      const block = await a.json();
-      const transfer = await b.json();
-      const total = await c.json();
-      const staking = await d.json();
-      const totalLock = await e.json();
-      
-      setLoading(false);
-      setOverview({
-        block,
-        transfer,
-        total,
-        staking,
-        totalLock
-      })
-    })
-    .catch(err => {
-      setLoading(false);
-      console.log(err);
-    })
-  },[blockNumber]);
+      .then(async ([a, b, c, d, e]) => {
+        const block = await a.json();
+        const transfer = await b.json();
+        const total = await c.json();
+        const staking = await d.json();
+        const totalLock = await e.json();
 
-  if(loading) return (
-    <div className="container">
-      <Loading />
-    </div>
-  )
+        setLoading(false);
+        setOverview({
+          block,
+          transfer,
+          total,
+          staking,
+          totalLock,
+        });
+      })
+      .catch((err) => {
+        setLoading(false);
+        console.log(err);
+      });
+  }, [blockNumber]);
+
+  if (loading)
+    return (
+      <div className="container">
+        <Loading />
+      </div>
+    );
 
   return (
     <div>
@@ -83,7 +84,7 @@ export default function Home() {
           <div className="spacing" />
           <Search />
           <div className="spacing" />
-          <Overview 
+          <Overview
             total_blocks={blockNumber}
             total_blocksFinalized={blockNumberFinalized}
             total_extrinsicSigned={overview?.total.SignedExtrinsic}
