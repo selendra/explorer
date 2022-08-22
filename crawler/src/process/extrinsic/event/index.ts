85d3289f4f4bf5ca8bb2e8f221c6017a230b1503
LayNath242 <nath.selendra@gmail.com>
2022-08-22 12:03:23 +0700

    add reward payout event

--- crawler/src/process/extrinsic/event/index.ts
+++ crawler/src/process/extrinsic/event/index.ts
@@ -1,10 +1,11 @@
-import { EventData } from '../../../types';
 import DefaultEvent from './DefaultEvent';
 import EndowedEvent from './EndowedEvent';
 import ReservedEvent from './ReservedEvent';
+import StakingEvent from './StakingEvent';
 import ClaimEvmAccountEvent from './ClaimEvmAccountEvent';
-import { NativeTransferEvent } from './transfer';
 import KillAccountEvent from './KillAccountEvent';
+import { NativeTransferEvent } from './transfer';
+import { EventData } from '../../../types';
 
 const resolveEvent = async (head: EventData): Promise<DefaultEvent> => {
   // Compressing event section and method
@@ -16,6 +17,7 @@ const resolveEvent = async (head: EventData): Promise<DefaultEvent> => {
     case 'balances.Endowed': return new EndowedEvent(head);
     case 'balances.Reserved': return new ReservedEvent(head);
     case 'balances.Transfer': return new NativeTransferEvent(head);
+    case 'staking.Rewarded': return new StakingEvent(head);
     case 'system.KilledAccount': return new KillAccountEvent(head);
     default: return new DefaultEvent(head);
   }
