f3d81d137b7178f7154c329ff9d49264355e2164
LayNath242 <nath.selendra@gmail.com>
2022-08-22 10:53:40 +0700

    add kill account and claim evm account evnt

--- crawler/src/process/extrinsic/event/index.ts
+++ crawler/src/process/extrinsic/event/index.ts
@@ -2,7 +2,9 @@ import { EventData } from '../../../types';
 import DefaultEvent from './DefaultEvent';
 import EndowedEvent from './EndowedEvent';
 import ReservedEvent from './ReservedEvent';
+import ClaimEvmAccountEvent from './ClaimEvmAccountEvent';
 import { NativeTransferEvent } from './transfer';
+import KillAccountEvent from './KillAccountEvent';
 
 const resolveEvent = async (head: EventData): Promise<DefaultEvent> => {
   // Compressing event section and method
@@ -10,9 +12,11 @@ const resolveEvent = async (head: EventData): Promise<DefaultEvent> => {
 
   // Decoding native events
   switch (eventCompression) {
+    case 'evmAccounts.ClaimAccount': return new ClaimEvmAccountEvent(head);
     case 'balances.Endowed': return new EndowedEvent(head);
     case 'balances.Reserved': return new ReservedEvent(head);
     case 'balances.Transfer': return new NativeTransferEvent(head);
+    case 'system.KilledAccount': return new KillAccountEvent(head);
     default: return new DefaultEvent(head);
   }
 };
