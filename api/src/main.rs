f7792e434356c2c75010739e18a1062de2c889e3
Nath242 <nath.selendra@gmail.com>
2024-10-29 11:04:06 +0700

    update

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,26 +1,25 @@
 use dotenv::dotenv;
 use std::env;
-use actix_web::{middleware, web, App, HttpRequest, HttpServer};
+use actix_web::{get, middleware, web, App, HttpServer, Responder};
 
-async fn index(req: HttpRequest) -> &'static str {
-    println!("REQ: {req:?}");
-    "Hello world!"
+#[get("/hello/{name}")]
+async fn greet(name: web::Path<String>) -> impl Responder {
+    format!("Hello {name}!")
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     dotenv().ok();
 
-    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
-    log::info!("starting HTTP server at http://localhost:8080");
-    
     let port = env::var("PORT").as_deref().unwrap_or("8080").parse::<u16>().expect("Should be number: 8080");
 
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+    log::info!("starting HTTP server at http://localhost:{port}");
+    
     HttpServer::new(|| {
         App::new()
             .wrap(middleware::Logger::default())
-            .service(web::resource("/index.html").to(|| async { "Hello world!" }))
-            .service(web::resource("/").to(index))
+            .service(greet)
     })
     .bind(("127.0.0.1", port))?
     .run()
