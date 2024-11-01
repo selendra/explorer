c57fa5b348ef4b730de28d1b659a8bf81aac6a1f
Nath242 <nath.selendra@gmail.com>
2024-11-01 23:23:33 +0700

    fetching block data

--- rust-client/src/chain_state/substrate.rs
+++ rust-client/src/chain_state/substrate.rs
@@ -1,13 +1,17 @@
-use anyhow::{Context, Result};
+use anyhow::{anyhow, Context, Result};
+use serde::{Deserialize, Serialize};
 
 use substrate_api_client::{
 	ac_primitives::DefaultRuntimeConfig, rpc::JsonrpseeClient, Api, GetChainInfo,
 };
-
-// struct BlockDetail {
-//     block_number: u128,
-//     block_hash: String,
-// }
+#[derive(Debug, Serialize, Deserialize)]
+pub struct BlockDetail {
+	block_number: u128,
+	block_hash: String,
+	parent_hash: String,
+	extrinsics_root: String,
+	state_root: String,
+}
 
 pub struct SubstrateClient {
 	ws_url: String,
@@ -21,12 +25,12 @@ impl SubstrateClient {
 
 	pub async fn connect(&mut self) -> Result<()> {
 		let client = JsonrpseeClient::new(&self.ws_url)
-			.map_err(|e| anyhow::anyhow!("Failed to connect to WebSocket: {:?}", e))?;
+			.map_err(|e| anyhow!("Failed to connect to WebSocket: {:?}", e))?;
 
 		self.api = Some(
 			Api::<DefaultRuntimeConfig, _>::new(client)
 				.await
-				.map_err(|e| anyhow::anyhow!("Failed to create API: {:?}", e))?,
+				.map_err(|e| anyhow!("Failed to create API: {:?}", e))?,
 		);
 
 		Ok(())
@@ -35,11 +39,29 @@ impl SubstrateClient {
 	pub async fn get_block(&self) -> Result<()> {
 		let api = self.api.as_ref().context("API client not initialized")?;
 
-		let block_hash = api.get_block_hash(Some(1)).await.unwrap();
+		let block_hash = api
+			.get_block_hash(Some(1))
+			.await
+			.map_err(|e| anyhow!("Fetching blockHash Error{:?}", e))?;
 
-		let block = api.get_block(block_hash).await.unwrap();
+		let block = api
+			.get_block(block_hash)
+			.await
+			.map_err(|e| anyhow!("Fetching Block Error{:?}", e))?;
 
-		println!("{:?}", block);
+		if let Some(block) = block {
+			let block_data = BlockDetail {
+				block_number: block.header.number as u128,
+				block_hash: block.header.hash().to_string(),
+				parent_hash: block.header.parent_hash.to_string(),
+				extrinsics_root: block.header.extrinsics_root.to_string(),
+				state_root: block.header.state_root.to_string()
+			};
+
+			println!("{:?}", block_data);
+		} else {
+			println!("None");
+		}
 		Ok(())
 	}
 }
