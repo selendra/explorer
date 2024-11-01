f273e27d162ee75b880aa1a72a3f29b5cdc57f9b
Nath242 <nath.selendra@gmail.com>
2024-11-01 22:23:10 +0700

    sdk

--- selendra-sdk/src/index.ts
+++ selendra-sdk/src/index.ts
@@ -8,17 +8,17 @@ import { SubstrateChainState, EvmChainState } from './chain_state';
 // eslint-disable-next-line @typescript-eslint/no-empty-function
 async function main() {
   const selendraApi = new SelendraApi('https://rpc.selendra.org');
-  let api = await selendraApi.subtrateProvider();
-  const substrate = new SubstrateChainState(api);
-  // const evm = new EvmChainState(api);
-  // console.log(evm);
+  let api = await selendraApi.evmProvider();
+  // const substrate = new SubstrateChainState(api);
+  const evm = new EvmChainState(api);
+  console.log(await evm.getBlockDetails(7687398));
+  // console.log(await evm.getTransaction(7687398));
 
   // console.log(await substrate.getBlockData(7_424_891));
   // await substrate.getBlockEvent(7_431_654);
   // console.log(await substrate.getExtrinsicDetails(7577752));
   // console.log(await substrate.getAccontBalanceInfo('5D2zu5mBct9CwjLhCMkz2xTXHWzG2GmDk4tYeBjm5MaYwuV7'));
-  console.log(await substrate.getAllAccounts());
-  api.disconnect();
+  // api.disconnect();
 }
 
 // Execute the main function and handle any errors
