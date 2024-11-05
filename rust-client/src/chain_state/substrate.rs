95424a692eeec57eb0eabe59f0e131ddfce62ed9
Nath242 <nath.selendra@gmail.com>
2024-11-05 18:47:24 +0700

    add transfer, slash event and optimize code

--- rust-client/src/chain_state/substrate.rs
+++ rust-client/src/chain_state/substrate.rs
@@ -1,22 +1,22 @@
-use anyhow::{anyhow, Context, Result};
+use anyhow::{anyhow, Result};
 use codec::{Decode, Encode};
 
 use pallet_staking::ActiveEraInfo;
 use substrate_api_client::{
-	ac_primitives::{Bytes, DefaultRuntimeConfig},
+	ac_primitives::{Bytes, DefaultRuntimeConfig, RuntimeVersion},
 	rpc::JsonrpseeClient,
 	Api, GetChainInfo, GetStorage, GetTransactionPayment,
 };
 
 use frame_system::Phase;
-use selendra_primitives::Signature;
-use selendra_runtime::{Address, RuntimeCall, SignedExtra};
+use selendra_primitives::{Hash, Signature};
+use selendra_runtime::{Address, RuntimeCall, RuntimeEvent, SignedExtra};
 use sp_core::{blake2_256, crypto::Ss58Codec};
-use sp_runtime::generic::UncheckedExtrinsic;
+use sp_runtime::{generic::{Block, Header, UncheckedExtrinsic}, traits::BlakeTwo256, OpaqueExtrinsic};
 
 use crate::models::{
-	block::{BlockDetail, RuntimeVersion},
-	event::{EventDetail, SubstrateEventRecord, BlockEvent},
+	block::{BlockDetail, SubstrateRuntimeVersion},
+	event::{BlockEvent, EventDetail, StakingSlash, SubstrateEventRecord, TransferEvent},
 	extrinsic::{BlockExtrinsic, ExtrinsicDetail, ProcessExtrinsic},
 	Balance,
 };
@@ -31,74 +31,60 @@ impl SubstrateClient {
 		Self { ws_url: ws_url.to_string(), api: None }
 	}
 
+	/// Connects to the Substrate node via WebSocket and initializes the API.
 	pub async fn connect(&mut self) -> Result<()> {
 		let client = JsonrpseeClient::new(&self.ws_url)
-			.map_err(|e| anyhow!("Failed to connect to WebSocket: {:?}", e))?;
+			.map_err(|e| anyhow!("Failed to connect to WebSocket at {}: {:?}", self.ws_url, e))?;
 
 		self.api = Some(
 			Api::<DefaultRuntimeConfig, _>::new(client)
 				.await
-				.map_err(|e| anyhow!("Failed to create API: {:?}", e))?,
+				.map_err(|e| anyhow!("Failed to create API client: {:?}", e))?,
 		);
 
 		Ok(())
 	}
 
-	pub async fn get_block(&self, block_numer: u32) -> Result<Option<BlockDetail>> {
-		let api = self.api.as_ref().context("API client not initialized")?;
+	/// Retrieves the block details for the given block number.
+	pub async fn get_block(&self, block_number: u32) -> Result<Option<BlockDetail>> {
+		let api = self.api.as_ref().ok_or_else(|| anyhow!("API client not initialized"))?;
 
+		let block_hash = self.get_block_hash(api, block_number).await?;
 		let runtime_version = api.runtime_version();
 
-		let block_hash = api
-			.get_block_hash(Some(block_numer))
-			.await
-			.map_err(|e| anyhow!("Fetching blockHash Error{:?}", e))?;
-
-		let (block, era_info, session_index, total_issuance) = futures::future::try_join4(
+		let (
+			block,
+			era_info,
+			session_index,
+			total_issuance,
+			event_count,
+			extrinsic_count,
+			timestamp,
+		) = futures::try_join!(
 			api.get_block(block_hash),
 			api.get_storage::<ActiveEraInfo>("Staking", "ActiveEra", block_hash),
 			api.get_storage::<u32>("Session", "CurrentIndex", block_hash),
 			api.get_storage::<Balance>("Balances", "TotalIssuance", block_hash),
-		)
-		.await
-		.map_err(|e| anyhow::anyhow!("feching Block data error: {:?}", e))?;
-
-		let (event_count, extrinsic_count, timestamp) = futures::future::try_join3(
 			api.get_storage::<u32>("System", "EventCount", block_hash),
 			api.get_storage::<u32>("System", "ExtrinsicCount", block_hash),
 			api.get_storage::<u64>("Timestamp", "Now", block_hash),
 		)
-		.await
 		.map_err(|e| anyhow!("feching data error: {:?}", e))?;
 
 		if let Some(block) = block {
-			
-			let mut block_extrinsic: Vec<ExtrinsicDetail> = Vec::new();
-			for (index, extrinsic) in block.extrinsics.iter().enumerate() {
-				let extrinsic_byte = extrinsic.encode();
-				let extrinsic_hash = format!("0x{}", hex::encode(blake2_256(&extrinsic_byte)));
-				let fee_details = api
-					.get_fee_details(&Bytes(extrinsic_byte.clone()), block_hash)
-					.await
-					.map_err(|e| anyhow!("feching fee error {:?}", e))?;
+			let block_extrinsics = self.process_extrinsics(api, block.clone(), block_hash).await?;
 
-				let total_fee = fee_details.map(|fee| fee.final_fee()).unwrap_or_else(|| 0);
-				let is_signed = total_fee > 0;
+			let events = api
+				.get_storage::<Vec<SubstrateEventRecord>>("System", "Events", block_hash)
+				.await
+				.map_err(|e| anyhow!("Failed to create API client: {:?}", e))?;
 
-				let process_extrinsic = self.process_extrinsic(extrinsic_byte.clone())?;
-
-				block_extrinsic.push(ExtrinsicDetail {
-					is_signed,
-					signer: process_extrinsic.signer,
-					index: index as u8,
-					hash: extrinsic_hash,
-					fee: total_fee,
-					runtime_call: format!("{:?}", process_extrinsic.function).to_string(),
-				});
-			}
-
-			let events = api.get_storage::<Vec::<SubstrateEventRecord>>("System", "Events", block_hash).await.unwrap().unwrap();
-			let block_event = self.process_event(events)?;
+			let block_events = match events {
+				Some(event_data) => self.process_event(event_data)?,
+				None => {
+					Vec::new()
+				}
+			};
 
 			let block_data = BlockDetail {
 				block_number: block.header.number,
@@ -106,33 +92,67 @@ impl SubstrateClient {
 				parent_hash: block.header.parent_hash.to_string(),
 				extrinsics_root: block.header.extrinsics_root.to_string(),
 				activ_era: era_info.map_or(0, |era| era.index),
-				session_index: session_index.map_or(0, |index| index),
+				session_index: session_index.unwrap_or_default(),
 				state_root: block.header.state_root.to_string(),
-				runtime_version: RuntimeVersion {
-					spec_name: runtime_version.spec_name.to_string(),
-					impl_name: runtime_version.impl_name.to_string(),
-					authoring_version: runtime_version.authoring_version,
-					spec_version: runtime_version.spec_version,
-					impl_version: runtime_version.impl_version,
-					transaction_version: runtime_version.transaction_version,
-					state_version: runtime_version.state_version,
-				},
+				runtime_version: self.build_runtime_version(runtime_version),
 				events: BlockEvent {
-					total: event_count.map_or(0, |total| total),
-					extrinsic: block_event
+					total: event_count.unwrap_or_default(),
+					extrinsic: block_events,
 				},
 				extrinsics: BlockExtrinsic {
-					total: extrinsic_count.map_or(0, |total| total),
-					extrinsic: block_extrinsic,
+					total: extrinsic_count.unwrap_or_default(),
+					extrinsic: block_extrinsics,
 				},
-				total_issuance: total_issuance.map_or(0, |total| total),
-				timestamp: timestamp.map_or(0, |time| time),
+				total_issuance: total_issuance.unwrap_or_default(),
+				timestamp: timestamp.unwrap_or_default(),
 			};
 
-			return Ok(Some(block_data))
+			Ok(Some(block_data))
 		} else {
-			return Ok(None)
+			Ok(None)
 		}
+	}
+
+	async fn get_block_hash(
+		&self,
+		api: &Api<DefaultRuntimeConfig, JsonrpseeClient>,
+		block_number: u32,
+	) -> Result<Option<Hash>> {
+		api.get_block_hash(Some(block_number)).await.map_err(|e| {
+			anyhow!("Failed to fetch block hash for block number {}: {:?}", block_number, e)
+		})
+	}
+
+	async fn process_extrinsics(
+		&self,
+		api: &Api<DefaultRuntimeConfig, JsonrpseeClient>,
+		block:Block<Header<u32, BlakeTwo256>, OpaqueExtrinsic>,
+		block_hash: Option<Hash>,
+	) -> Result<Vec<ExtrinsicDetail>> {
+		let mut block_extrinsics = Vec::new();
+
+		for (index, extrinsic) in block.extrinsics.iter().enumerate() {
+			let extrinsic_byte = extrinsic.encode();
+			let extrinsic_hash = format!("0x{}", hex::encode(blake2_256(&extrinsic_byte)));
+			let fee_details = api
+				.get_fee_details(&Bytes(extrinsic_byte.clone()), block_hash)
+				.await
+				.map_err(|e| anyhow!("Fetching fee details failed: {:?}", e))?;
+			let total_fee = fee_details.map_or(0, |fee| fee.final_fee());
+			let is_signed = total_fee > 0;
+
+			let processed_extrinsic = self.process_extrinsic(extrinsic_byte)?;
+
+			block_extrinsics.push(ExtrinsicDetail {
+				is_signed,
+				signer: processed_extrinsic.signer,
+				index: index as u8,
+				hash: extrinsic_hash,
+				fee: total_fee,
+				runtime_call: format!("{:?}", processed_extrinsic.function),
+			});
+		}
+		Ok(block_extrinsics)
 	}
 
 	fn process_extrinsic(&self, extrinsic_byte: Vec<u8>) -> Result<ProcessExtrinsic> {
@@ -140,38 +160,68 @@ impl SubstrateClient {
 			UncheckedExtrinsic<Address, RuntimeCall, Signature, SignedExtra>,
 			_,
 		> = Decode::decode(&mut &extrinsic_byte[..])
-			.map_err(|e| anyhow!("decode extrinis error: {:?}", e));
+			.map_err(|e| anyhow!("Decoding extrinsic failed: {:?}", e));
 
-		let signer_address = if let Ok(ref extrinsic) = decoded_extrinsic {
-			extrinsic
-				.signature
-				.as_ref()
-				.map(|(signer, _, _)| signer.to_ss58check())
-				.unwrap_or_default()
-		} else {
-			String::new()
-		};
+		let signer_address = decoded_extrinsic
+			.as_ref()
+			.ok()
+			.and_then(|extrinsic| {
+				extrinsic.signature.as_ref().map(|(signer, _, _)| signer.to_ss58check())
+			})
+			.unwrap_or_default();
 
 		Ok(ProcessExtrinsic { signer: signer_address, function: decoded_extrinsic?.function })
 	}
 
 	fn process_event(&self, events: Vec<SubstrateEventRecord>) -> Result<Vec<EventDetail>> {
-		let indexed_block_events: Vec<(usize, SubstrateEventRecord)> =
-			events.into_iter().enumerate().collect();
+		Ok(events
+			.into_iter()
+			.enumerate()
+			.map(|(index, event)| {
+				let phase = match event.phase {
+					Phase::ApplyExtrinsic(index) => index,
+					_ => 0,
+				};
 
-		let mut block_events: Vec<EventDetail> = Vec::new();
-		for (index, event) in indexed_block_events {
-			let phase = match event.phase {
-				Phase::ApplyExtrinsic(index) => index,
-				_ => 0,
-			};
+				EventDetail {
+					index: index as u32,
+					extrinsic_id: phase,
+					event: format!("{:?}", event.event),
+					slash_event: self.process_slash_event(event.event.clone()).unwrap_or(None),
+					transfer_event: self
+						.process_transfer_event(event.event.clone())
+						.unwrap_or(None),
+				}
+			})
+			.collect())
+	}
 
-			block_events.push(EventDetail {
-				index: index as u32,
-				extrinsic_id: phase,
-				event: format!("{:?}", event.event),
-			});
+	fn process_slash_event(&self, event: RuntimeEvent) -> Result<Option<StakingSlash>> {
+		if let RuntimeEvent::Staking(pallet_staking::Event::Slashed { staker, amount }) = event {
+			Ok(Some(StakingSlash { staker: staker.to_ss58check(), amount }))
+		} else {
+			Ok(None)
 		}
-		Ok(block_events)
+	}
+
+	fn process_transfer_event(&self, event: RuntimeEvent) -> Result<Option<TransferEvent>> {
+		if let RuntimeEvent::Balances(pallet_balances::Event::Transfer { from, to, amount }) = event
+		{
+			Ok(Some(TransferEvent { from: from.to_ss58check(), to: to.to_ss58check(), amount }))
+		} else {
+			Ok(None)
+		}
+	}
+
+	fn build_runtime_version(&self, runtime_version: &RuntimeVersion) -> SubstrateRuntimeVersion {
+		SubstrateRuntimeVersion {
+			spec_name: runtime_version.spec_name.to_string(),
+			impl_name: runtime_version.impl_name.to_string(),
+			authoring_version: runtime_version.authoring_version,
+			spec_version: runtime_version.spec_version,
+			impl_version: runtime_version.impl_version,
+			transaction_version: runtime_version.transaction_version,
+			state_version: runtime_version.state_version,
+		}
 	}
 }
--- rust-client/src/models/event.rs
+++ rust-client/src/models/event.rs
@@ -19,10 +19,25 @@ pub struct EventDetail {
 	pub index: u32,
 	pub extrinsic_id: u32,
 	pub event: String,
+	pub slash_event: Option<StakingSlash>,
+	pub transfer_event: Option<TransferEvent>,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct BlockEvent {
 	pub total: u32,
 	pub extrinsic: Vec<EventDetail>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct StakingSlash {
+	pub staker: String,
+	pub amount: u128,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct TransferEvent {
+	pub from: String,
+	pub to: String,
+	pub amount: u128,
 }
